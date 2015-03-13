86d142d3bb3b91e1a6c181ed32d4a18004779c44
8shashank <8shashank@gmail.com>
2015-03-13 05:36:34 -0500

    Soln 8 without using external modules

--- 8.js
+++ 8.js
@@ -1,1 +1,14 @@
 var http=require('http');
+var url=process.argv[2];
+http.get(url,function (response){
+	response.setEncoding("utf8")
+	var buffer="";
+	response.on("data",function(chunk){
+		buffer+=chunk;
+	});
+	response.on("error",console.error);
+	response.on("end",function(){
+		console.log(buffer.length);
+		console.log(buffer);
+	});
+});
