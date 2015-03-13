0a4116341e79b1f0455f6199fb63a95cfc53039b
shashank_sharma_ <8shashank@gmail.com>
2015-03-13 15:03:07 -0600

    Added solution 11

--- 10.js
+++ 10.js
@@ -3,5 +3,5 @@ var strftime=require('strftime');
 
 var server=net.createServer(function (socket) {
     socket.end(strftime("%Y-%m-%d %k:%M"));
-});
+}); //TCP server
 server.listen(process.argv[2]);
